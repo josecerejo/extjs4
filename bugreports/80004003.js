8e43c2d459a5a43792babec5fd2c5693f97eb9dd
oger000 <gerhard.oettl@ogersoft.at>
2011-10-29 20:09:58 +0200

    additional testing on bugreports

--- bugreports/80004003.js
+++ bugreports/80004003.js
@@ -1,4 +1,24 @@
 
+
+var formDef1 = {
+  layout: 'anchor',
+  items: [
+    { fieldLabel: 'Label1', name: 'name', xtype: 'textfield', allowBlank: false },
+    { fieldLabel: 'Label2', name: 'other', xtype: 'textfield' },
+  ],
+};
+
+
+var formDef2 = Ext.create('Ext.form.Panel', {
+  //xtype: 'form',
+  layout: 'anchor',
+  items: [
+    { fieldLabel: 'Label1', name: 'name', xtype: 'textfield', allowBlank: false },
+    { fieldLabel: 'Label2', name: 'other', xtype: 'textfield' },
+  ],
+});
+
+
 Ext.define('App.view.Form', {
   extend: 'Ext.form.Panel',
   alias: 'widget.myform',
@@ -20,10 +40,12 @@ Ext.define('App.view.Window', {
   modal: true,
   layout: 'fit',
   //   following line of code works only for FIRST instance created with Ext.create('App.view.Window')
-  //   the second instance is empty (no form)
+  //   the second instance is empty (form is here but looses its items!)
   //   extjs 4.0.7  (with 4.0.2a i got an 80004003 error)
   // items: [ Ext.create('App.view.Form') ],
-  items: [ { xtype: 'myform' } ],  // this works as expected
+  // items: [ { xtype: 'myform' } ],  // this works as expected
+  // items: [ Ext.create('Ext.form.Panel', formDef1) ],  // this works only once like: Ext.create('App.view.Form')
+  items: [ formDef2 ],  // this works only once like: Ext.create('App.view.Form')
   buttonAlign: 'center',
   buttons: [
     { text: 'Ok',
@@ -49,7 +71,8 @@ Ext.define('App.view.Viewport', {
         { xtype: 'button',
           text: 'Show Window',
           handler: function(button, event) {
-            Ext.create('App.view.Window').show();
+            var win = Ext.create('App.view.Window').show();
+            var x = x;
           },
         },
       ],
